a46f984cf6ceae12605bb3e028c640c6fae97ac0
Pablo Cazorla <pablo.david.cazorla@gmail.com>
2015-05-17 22:25:30 -0300

    start

---
+++ index.php
@@ -0,0 +1,93 @@
+<!doctype HTML>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>UI-maker</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	
+	<!-- CSS -->
+	<link type="text/css" rel="stylesheet" href="css/style.css">
+
+	<!--[if lt IE 9]>
+	<script src="js/libs/html5-3.4-respond-1.1.0.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<div id="element-menu">
+		<div id="element-menu-title">Elements</div>
+		<ul id="element-menu-list">
+			<li class="">
+				<span class="element-menu-selector"></span><a href="#button">Button</a>
+			</li>
+			<li class="selected">
+				<span class="element-menu-selector"></span><a href="#button">Button</a>
+			</li>
+			<li class="">
+				<span class="element-menu-selector"></span><a href="#button">Button</a>
+			</li>
+		</ul>
+	</div>
+	<div id="element-screen">
+		<div class="wrap" id="page">
+			<div class="element-module">
+				<div class="element-module-title">Typhography</div>
+				sdfsdfsdfsdf
+			</div>
+			<div class="element-module">
+				<div class="element-module-title">Lists</div>
+				sdfsdfsdfsdf
+			</div>
+			<div class="element-module">
+				<div class="element-module-title">Tables</div>
+				sdfsdfsdfsdf
+			</div>
+			<div class="element-module">
+				<div class="element-module-title">Pre</div>
+				sdfsdfsdfsdf
+			</div>
+			<div class="element-module">
+				<div class="element-module-title">Iframe</div>
+				sdfsdfsdfsdf
+			</div>
+			<div class="element-module">
+				<div class="element-module-title">Canvas</div>
+				sdfsdfsdfsdf
+			</div>
+			<div class="element-module">
+				<div class="element-module-title">Buttons</div>
+				sdfsdfsdfsdf
+			</div>
+			<div class="element-module">
+				<div class="element-module-title">Button Group</div>
+				sdfsdfsdfsdf
+			</div>
+			<div class="element-module">
+				<div class="element-module-title">Dropdown Group</div>
+				sdfsdfsdfsdf
+			</div>
+			<div class="element-module">
+				<div class="element-module-title">Form basic</div>
+				sdfsdfsdfsdf
+			</div>
+			<div class="element-module">
+				<div class="element-module-title">Form advance</div>
+				sdfsdfsdfsdf
+			</div>
+			<div class="element-module">
+				<div class="element-module-title">Tabs</div>
+				sdfsdfsdfsdf
+			</div>
+			<div class="element-module">
+				<div class="element-module-title">Modal</div>
+				sdfsdfsdfsdf
+			</div>
+		</div>
+		
+	</div>
+	<script src="js/libs/jquery-1.11.2.min.js"></script>
+	<script src="js/app.js"></script>
+</body>
